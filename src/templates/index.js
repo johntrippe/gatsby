ffcc9f5d43e63e935808e3f6dff4eaef1622c14e
John Trippe <johnetrippe@gmail.com>
2019-10-03 16:28:23 -0700

    Embed Content

--- src/templates/index.js
+++ src/templates/index.js
@@ -21,6 +21,7 @@ const Index = ({ data, pageContext }) => {
       {!isFirstPage && (
         <Helmet>
           <title>{`${config.siteTitle} - Page ${currentPage}`}</title>
+          <script src="https://cdn.embedly.com/widgets/platform.js" type="text/javascript"/>
         </Helmet>
       )}
       <Container>
