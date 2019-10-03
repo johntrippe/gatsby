a1ef7917a547c454a062f2464e99334c34cea321
John Trippe <johnetrippe@gmail.com>
2019-10-03 16:49:46 -0700

    embed

--- src/templates/page.js
+++ src/templates/page.js
@@ -15,7 +15,18 @@ const PageTemplate = ({ data }) => {
   return (
     <Layout>
       <Helmet>
-        <script src="https://cdn.embedly.com/widgets/platform.js" type="text/javascript"/>
+        <script>
+  (function(w, d){
+   var id='embedly-platform', n = 'script';
+   if (!d.getElementById(id)){
+     w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+     var e = d.createElement(n); e.id = id; e.async=1;
+     e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+     var s = d.getElementsByTagName(n)[0];
+     s.parentNode.insertBefore(e, s);
+   }
+  })(window, document);
+</script>
         <title>{`${title} - ${config.siteTitle}`}</title>
       </Helmet>
       <SEO pagePath={slug} postNode={postNode} pageSEO />
