5b54401868882b0712fe0d0296d9c355c79e6588
John Trippe <johnetrippe@gmail.com>
2019-10-18 12:01:17 -0700

    test

--- src/templates/post.js
+++ src/templates/post.js
@@ -51,6 +51,13 @@ export const query = graphql`
   query($slug: String!) {
     contentfulPost(slug: { eq: $slug }) {
       title
+      edges {
+      node {
+        bodyRichText {
+          json
+        }
+      }
+    }
       slug
       metaDescription {
         internal {
